edbcd366d62fba97f2ad72a7c647dad2c306bedc
kam4atka <sledsib@ya.ru>
2021-04-04 18:56:58 +0300

    Обработка событий

---
+++ js/user-modal.js
@@ -0,0 +1,47 @@
+import {isEscapeKey, isEnterKey} from './util.js';
+import {renderSimilarList, clearSimilarList} from './similar-list.js';
+
+const userModalElement = document.querySelector('.setup');
+const userModalOpenElement = document.querySelector('.setup-open');
+const userModalCloseElement = userModalElement.querySelector('.setup-close');
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+function openUserModal () {
+  userModalElement.classList.remove('hidden');
+  renderSimilarList();
+
+  document.addEventListener('keydown', onPopupEscKeydown);
+}
+
+function closeUserModal () {
+  userModalElement.classList.add('hidden');
+  clearSimilarList();
+
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
+
+userModalOpenElement.addEventListener('click', () => {
+  openUserModal();
+});
+
+userModalOpenElement.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    openUserModal();
+  }
+});
+
+userModalCloseElement.addEventListener('click', () => {
+  closeUserModal();
+});
+
+userModalCloseElement.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    closeUserModal();
+  }
+});
--- js/util.js
+++ js/util.js
@@ -10,4 +10,8 @@ const getRandomPositiveInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-export {getRandomArrayElement};
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+export {getRandomArrayElement, isEscapeKey, isEnterKey};

7f1eb5cbcb9675be46f841f0b655bb54ac208c66
kam4atka <sledsib@ya.ru>
2021-04-05 14:31:07 +0300

    Устранение дребезга

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ import {closeUserModal} from './user-modal.js';
 import {setUserFormSubmit, setEyesClick, setCoatClick} from './user-form.js';
 import {renderSimilarList} from './similar-list.js';
 import {getData} from './api.js';
+import {debounce} from './util.js';
+
+const RERENDER_DELAY = 500;
 
 getData((wizards) => {
   renderSimilarList(wizards);
-  setEyesClick(() => renderSimilarList(wizards));
-  setCoatClick(() => renderSimilarList(wizards));
+  setEyesClick(debounce(
+    () => renderSimilarList(wizards),
+    RERENDER_DELAY,
+  ));
+  setCoatClick(debounce(
+    () => renderSimilarList(wizards),
+    RERENDER_DELAY,
+  ));
 });
 
 setUserFormSubmit(closeUserModal);

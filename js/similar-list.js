d341e2e0c63f20430bec937fe133b7adf165c9e0
kam4atka <sledsib@ya.ru>
2021-04-05 13:36:15 +0300

    Pimp my mage

--- js/similar-list.js
+++ js/similar-list.js
@@ -1,27 +1,23 @@
-import {createWizards} from './data.js';
-
 const similarElement = document.querySelector('.setup-similar');
-similarElement.classList.remove('hidden');
 
 const similarListElement = similarElement.querySelector('.setup-similar-list');
 const similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-const similarWizards = createWizards();
-
-const renderSimilarList = () => {
+const renderSimilarList = (similarWizards) => {
   const similarListFragment = document.createDocumentFragment();
 
-  similarWizards.forEach(({name, coatColor, eyesColor}) => {
+  similarWizards.forEach(({name, colorCoat, colorEyes}) => {
     const wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = name;
-    wizardElement.querySelector('.wizard-coat').style.fill = coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = colorEyes;
     similarListFragment.appendChild(wizardElement);
   });
 
   similarListElement.appendChild(similarListFragment);
+  similarElement.classList.remove('hidden');
 };
 
 const clearSimilarList = () => {

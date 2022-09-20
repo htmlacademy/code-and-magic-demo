7d8e95f026c451d166e3dc6cf342745f3a6c8aa0
Lydia Zakharova <jahontis@gmail.com>
2022-09-20 14:04:10 +0300

    Модульность

---
+++ js/util.js
@@ -0,0 +1,13 @@
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+export {getRandomArrayElement};
